4cfa986631acb5332dec5afb86f1fbd3d6dc4d12
vdack <thousandawn@126.com>
2024-03-05 15:53:36 +0800

    1st for project1

--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -1,8 +1,9 @@
 package gh2;
 
-// TODO: uncomment the following import once you're ready to start this portion
-// import deque.Deque;
-// TODO: maybe more imports
+//uncomment the following import once you're ready to start this portion
+ import deque.Deque;
+ import deque.LinkedListDeque;
+// maybe more imports
 
 //Note: This file will not compile until you complete the Deque implementations
 public class GuitarString {
@@ -13,15 +14,21 @@ public class GuitarString {
     private static final double DECAY = .996; // energy decay factor
 
     /* Buffer for storing sound data. */
-    // TODO: uncomment the following line once you're ready to start this portion
-    // private Deque<Double> buffer;
+    // uncomment the following line once you're ready to start this portion
+     private Deque<Double> buffer;
 
+     private int capacity;
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
         // TODO: Create a buffer with capacity = SR / frequency. You'll need to
         //       cast the result of this division operation into an int. For
         //       better accuracy, use the Math.round() function before casting.
         //       Your should initially fill your buffer array with zeros.
+        buffer = new LinkedListDeque<>();
+        capacity = (int) Math.round(SR / frequency);
+        for (int i = 0; i < capacity; i++){
+            buffer.addLast(0.0);
+        }
     }
 
 
@@ -35,6 +42,14 @@ public class GuitarString {
         //       other. This does not mean that you need to check that the numbers
         //       are different from each other. It means you should repeatedly call
         //       Math.random() - 0.5 to generate new random numbers for each array index.
+
+        double r;
+        for (int i = 0; i < capacity; i++){
+            r = Math.random() - 0.5;
+            buffer.removeFirst();
+            buffer.addLast(r);
+            System.out.print("random number: " + r);
+        }
     }
 
     /* Advance the simulation one time step by performing one iteration of
@@ -44,12 +59,16 @@ public class GuitarString {
         // TODO: Dequeue the front sample and enqueue a new sample that is
         //       the average of the two multiplied by the DECAY factor.
         //       **Do not call StdAudio.play().**
+        double num1 = buffer.removeFirst();
+        double num2 = buffer.getFirst();
+        double newNum = (num1 + num2) * DECAY / 2;
+        buffer.addLast(newNum);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
         // TODO: Return the correct thing.
-        return 0;
+        return buffer.getFirst();
     }
 }
     // TODO: Remove all comments that say TODO when you're done.
