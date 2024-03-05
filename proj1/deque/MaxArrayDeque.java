cbd9a2152b3f874d53601b1315f09f35e43b55ea
vdack <thousandawn@126.com>
2024-03-05 16:06:34 +0800

    2rd for project1

---
+++ proj1/deque/MaxArrayDeque.java
@@ -0,0 +1,35 @@
+package deque;
+
+import java.util.Comparator;
+
+public class MaxArrayDeque<T extends Comparator<T>> extends ArrayDeque<T>{
+
+    private Comparator<T> comparator;
+    public MaxArrayDeque(Comparator<T> c){
+        super();
+        this.comparator = c;
+    }
+    public T max(){
+        return preMax(comparator);
+    }
+
+    private T preMax(Comparator<T> comparator) {
+        T maxElement;
+        if(this.size() == 0){
+            return null;
+        }
+        else{
+            maxElement = this.getFirst();
+        }
+        for (T e : this){
+            if (comparator.compare(e,maxElement) == 1){
+                maxElement = e;
+            }
+        }
+        return maxElement;
+    }
+
+    public T max(Comparator<T> c){
+        return preMax((Comparator<T>) c);
+    }
+}
